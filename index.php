0e86b0a5cbb5c5941c60573093d2fafad610f446
Tim-VD-K <tim.vanderkloet@gmail.com>
2023-01-07 00:43:42 +0100

    Burger menu

--- index.php
+++ index.php
@@ -2,11 +2,19 @@
 <html lang="nl">
 
 <head>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/styles.css">
     <title>Home</title>
+
+    <script>
+        function MyFunction() {
+            document.getElementById("nav").classList.toggle("active")
+        }
+
+    </script>
 </head>
 
 <body>
@@ -14,8 +22,10 @@
         <div class="container">
             <!-- !heeft logo nodigz -->
             <h1 class="logo">Tim van der Kloet</h1>
-
-            <nav>
+            <a href="javascript:void(0);" class="icon" onclick="MyFunction()">
+                <i class="fa fa-bars"></i>
+            </a>
+            <nav id="nav">
                 <ul>
                     <li><a href="">Home</a></li>
                     <li><a href="about.php">About</a></li>
@@ -31,31 +41,21 @@
     </div>
     <!-- 3 projecten -->
     <div class="borderp">
-        <div class="project-home" id="home1">
+        <div class="project-home">
+            <img src="img/Gastenboek.png" class="img-size" alt="">
             <h1>Gastenboek</h1>
         </div>
-        <div class="project-home" id="home2">
+        <div class="project-home">
+            <img src="img/duckhunt.png" class="img-size" alt="">
             <h1>Duckhunt</h1>
         </div>
-        <div class="project-home" id="home3">
+        <div class="project-home">
+            <img src="img/radiogaga.png" class="img-size" alt="">
             <h1>RadioGaGa</h1>
         </div>
     </div>
 
-    <script>
-        // * naar andere pagina
-        document.getElementById("home1").onclick = function() {
-            location.href = "gastenboek/index.php";
-        };
 
-        document.getElementById("home2").onclick = function() {
-            location.href = "duckhunt/index.html";
-        };
-
-        document.getElementById("home3").onclick = function() {
-            location.href = "RadioGaGa/index.html";
-        };
-    </script>
 
 </body>
 
