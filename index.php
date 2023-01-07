f282fb8bac8da9e29e6250f9ddd10d6f6ad42f30
Tim-VD-K <tim.vanderkloet@gmail.com>
2023-01-07 12:04:41 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,24 +2,19 @@
 <html lang="nl">
 
 <head>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <title>Home</title>
 
-    <script>
-        function MyFunction() {
-            document.getElementById("nav").classList.toggle("active")
-        }
-    </script>
+
 </head>
 
 <body>
     <header>
         <div class="container">
-            <!-- !heeft logo nodigz -->
             <h1 class="logo">Tim van der Kloet</h1>
             <a href="javascript:void(0);" class="icon" onclick="MyFunction()">
                 <i class="fa fa-bars"></i>
@@ -54,8 +49,7 @@
         </div>
     </div>
 
-
-
+    <script src="js/nav.js"></script>
 </body>
 
 </html>
