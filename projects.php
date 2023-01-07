af31cb8ba23eea1f857af3ee3c1f4168af0a6876
Tim-VD-K <tim.vanderkloet@gmail.com>
2023-01-07 12:39:45 +0100

    Update projects.php

--- projects.php
+++ projects.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <title>Projects</title>
 
 </head>
@@ -15,7 +16,9 @@
         <div class="container">
 
             <h1 class="logo">Tim van der Kloet</h1>
-
+            <a href="javascript:void(0);" class="icon" onclick="MyFunction()">
+                <i class="fa fa-bars"></i>
+            </a>
             <nav>
                 <ul>
                     <li><a href="index.php">Home</a></li>
@@ -32,49 +35,27 @@
     </div>
 
     <div class="borderp">
-        <div class="project-home" id="pro1">
+        <div class="project-home">
+            <a href="gastenboek/index.php"><img src="img/Gastenboek.png" class="img-size" alt=""></a>
             <h1>Gastenboek</h1>
         </div>
-        <div class="project-home" id="pro2">
+        <div class="project-home">
+            <a href="duckhunt/index.html"><img src="img/duckhunt.png" class="img-size" alt=""></a>
             <h1>Duckhunt</h1>
         </div>
-        <div class="project-home" id="pro3">
+        <div class="project-home">
+            <a href="radiogaga/index.html"><img src="img/radiogaga.png" class="img-size" alt=""></a>
             <h1>RadioGaGa</h1>
         </div>
-        <div class="project-home" id="proj4">
-            <h1>Basic jQuery</h1>
-        </div>
-        <div class="project-home" id="proj5">
+        <div class="project-home">
+            <a href="gamecontroler/index.html"><img src="img/gamecon.png" class="img-size" alt=""></a>
             <h1>Game controler</h1>
         </div>
-        <div class="project-home" id="proj6">
-            <h1>Database (game)</h1>
+        <div class="project-home">
+            <a href="database/index.html"><img src="img/database.jpg" class="img-size" alt=""></a>
+            <h1>Game Database</h1>
         </div>
 </body>
-<script>
-    // * naar andere pagina
-    document.getElementById("pro1").onclick = function() {
-        location.href = "gastenboek/index.php";
-    };
-
-    document.getElementById("pro2").onclick = function() {
-        location.href = "duckhunt/index.html";
-    };
-
-    document.getElementById("pro3").onclick = function() {
-        location.href = "RadioGaGa/index.html";
-    };
-
-    document.getElementById("proj4").onclick = function() {
-        location.href = "BasicJqury/index.html";
-    };
-
-    document.getElementById("proj5").onclick = function() {
-        location.href = "gamecontroler/index.html";
-    };
-    document.getElementById("proj6").onclick = function() {
-        location.href = "database/index.php";
-    };
-</script>
+<script src="js/nav.js"></script>
 
 </html>
