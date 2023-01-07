4ff80ee3f47394e8fc49a4a4484e6287ba344f10
Tim-VD-K <tim.vanderkloet@gmail.com>
2023-01-07 21:57:02 +0100

    Update add-project.php

--- add-project.php
+++ add-project.php
@@ -1,22 +1,22 @@
 <?php
 
 if ($_POST) {
+    include 'uploadzip.php';
+
+    include 'upload.php';
 
     $con = new PDO("mysql:host=localhost;dbname=portfolio", "root", "");
     $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    $stmt = $con->prepare("INSERT INTO projecten(name, paht, img) VALUES (?, ?, ?)");
+    $stmt = $con->prepare("INSERT INTO projecten(name, path, img) VALUES (?, ?, ?)");
     $stmt->bindValue(1, htmlspecialchars($_POST["name"]));
-    $stmt->bindValue(2, htmlspecialchars($_POST["paht"]));
-    $stmt->bindValue(3, htmlspecialchars($_POST["img"]));
+    $stmt->bindValue(2, htmlspecialchars(substr($target_path, 0, -4) . "/index"));
+    $stmt->bindValue(3, htmlspecialchars($target_file));
 
     $stmt->execute();
 }
 
-
-
 ?>
-
 
 <!DOCTYPE html>
 <html lang="NL">
@@ -30,12 +30,18 @@ if ($_POST) {
 </head>
 
 <body>
-    <form action="" method="post">
-        <input type="text" id="name" name="name">
-        <input type="file" name="" id="">
+    <form method="post" enctype="multipart/form-data">
+        Naam van het project:
+        <input type="text" name="name" id="name"><br>
+        Github link:
+        <input type="link" name="github" id="github"><br>
+        Select image to upload:
+        <input type="file" name="img" id="img"><br>
+        Select your project
+        <input type="file" name="zip_file" id="img"><br>
+
+        <input type="submit" value="Toevoegen" name="submit">
     </form>
-
-
 </body>
 
 </html>
