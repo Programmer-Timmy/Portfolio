5e121159603a7f0380e2982cc67f26f05b81bd88
Tim-VD-K <tim.vanderkloet@gmail.com>
2023-01-08 22:50:21 +0100

    Update account.php

--- account.php
+++ account.php
@@ -17,7 +17,6 @@ if (!isset($_SESSION['loggedin'])) {
             if (password_verify($_POST['password'], $hash)) {
                 $hash = "";
                 $_SESSION['loggedin'] = true;
-                echo "Let me in, I'm genuine!";
             }
         }
     }
@@ -34,21 +33,23 @@ if (!isset($_SESSION['loggedin'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <title>account</title>
 </head>
 
 <body>
     <?php
     if (!isset($_SESSION['loggedin'])) {
-        echo '<form action="" method="post">
+        echo '<div class="admin"><form action="" method="post">
             username<br>
             <input type="text" name="username" id="username"><br>
             password<br>
             <input type="password" name="password" id="password"><br>
             <input type="submit" value="Inlogen">
-            </form>';
+            </form></div>';
     } else {
-        echo '<a href="javascript:void(0);" class="icon" onclick="MyFunction()">
+        echo '<header><div class="container"><a href="javascript:void(0);" class="icon" onclick="MyFunction()">
         <i class="fa fa-bars"></i>
         </a>
         <nav id="nav">
@@ -57,11 +58,17 @@ if (!isset($_SESSION['loggedin'])) {
             <li><a href="add-project">Add project</a></li>
             <li><a href="remove">Remove</a></li>
         </ul>
-        </nav>
+        </nav></div></header>
+        <div class="welcome">
+        <h1>Welcome!</h1>
+        <h2>You are loged in!</h2>
+        </div>
+
         ';
     }
     ?>
-    
+
 </body>
 <script src="js/nav.js"></script>
+
 </html>
