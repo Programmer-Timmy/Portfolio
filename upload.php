f1c7533e9917d4717831ff7dc28f536a4050a221
Tim van der Kloet <112855395+Tim-VD-K@users.noreply.github.com>
2023-01-09 16:33:58 +0100

    Update upload.php

--- upload.php
+++ upload.php
@@ -8,35 +8,35 @@ $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 if(isset($_POST["submit"])) {
   $check = getimagesize($_FILES["img"]["tmp_name"]);
   if($check !== false) {
-    echo "File is an image - " . $check["mime"] . ".";
     $uploadOk = 1;
   } else {
-    echo "File is not an image.";
+    echo "<script>alert('File is not an image');</script>";
     $uploadOk = 0;
   }
 }
 
 // Check if file already exists
 if (file_exists($target_file)) {
-  echo "Sorry, file already exists.";
+  echo "<script>alert('file already exists');</script>";
   $uploadOk = 0;
 }
 
 // Allow certain file formats
 if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
 && $imageFileType != "gif" ) {
-  echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+  echo "<script>alert('Sorry, only JPG, JPEG, PNG & GIF files are allowed');</script>";
   $uploadOk = 0;
 }
 
 // Check if $uploadOk is set to 0 by an error
 if ($uploadOk == 0) {
+  echo "<script>Sorry, your file was not uploaded');</script>";
   echo "Sorry, your file was not uploaded.";
 // if everything is ok, try to upload file
 } else {
   if (move_uploaded_file($_FILES["img"]["tmp_name"], $target_file)) {
-    echo "The file ". htmlspecialchars( basename( $_FILES["img"]["name"])). " has been uploaded.";
+    echo "<script>alert('The file has been uploaded.');</script>";
   } else {
-    echo "Sorry, there was an error uploading your file.";
+  echo "<script>Sorry, there was an error uploading your file');</script>";
   }
 }
