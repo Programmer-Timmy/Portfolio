e2220d9a1a2bb72bcacb7b77ce10eb5489931a40
Tim van der Kloet <112855395+Tim-VD-K@users.noreply.github.com>
2023-01-10 11:39:15 +0100

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,8 +1,5 @@
 <?php
-$target_dir = "img/";
-$target_file = $target_dir . basename($_FILES["img"]["name"]);
 $uploadOk = 1;
-$imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
 
 // Check if image file is a actual image or fake image
 if (isset($_POST["submit"])) {
