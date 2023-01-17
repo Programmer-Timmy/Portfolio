2b059274dfbc5920da344a7a6a8e090744815179
Tim van der Kloet <112855395+Tim-VD-K@users.noreply.github.com>
2023-01-17 13:50:58 +0100

    Update projects.php

--- projects.php
+++ projects.php
@@ -1,7 +1,7 @@
 <?php
 include_once('requierd.php');
 
-$project = $db->query('SELECT * FROM projecten ORDER BY date DESC')->fetchAll();
+$projects = $database->getRows('projecten', FALSE , FALSE, FALSE, 'date DESC');
 ?>
 
 <!DOCTYPE html>
@@ -39,13 +39,13 @@ $project = $db->query('SELECT * FROM projecten ORDER BY date DESC')->fetchAll();
     </div>
     <div class="borderp">
         <?php
-        foreach ($project as $project) {
+        foreach ($projects as $project) {
             echo "
             <div class='project-home'>
                 <a href=" . $project['path'] . "><img src=" . $project['img'] . " class='img-size' alt=''></a>";
 
             if ($project['github'] !== "empty") {
-                echo "<h1><a class='github' href=" . $project['github'] . "><i class='fa fa-github' aria-hidden='true'></i></a>$project->name</h1>";
+                echo "<h1><a class='github' href=" . $project['github'] . "><i class='fa fa-github' aria-hidden='true'></i></a>" . $project['name'] . "</h1>";
             } else {
                 echo "<h1>" . $project['name'] . "</h1>";
             }
