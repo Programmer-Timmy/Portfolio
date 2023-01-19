bb78d2976a5e3d13762906eb17e848f6c6faa75a
Tim van der Kloet <112855395+Tim-VD-K@users.noreply.github.com>
2023-01-19 12:14:39 +0100

    Update projects.php
    little clean up :)

--- classes/projects.php
+++ classes/projects.php
@@ -1,9 +1,9 @@
 <?php
 
-class Projects{
+class Projects {
 
     public static function loadprojects($limit = 3) {
-        $results = database::getRows('projecten', ['removed'] , 's', [0], 'date DESC LIMIT ' . $limit);
+        $results = database::getRows('projecten', ['removed'], 's', [0], 'date DESC LIMIT ' . $limit);
         if ($results) {
             return $results;
         } else {
@@ -12,7 +12,7 @@ class Projects{
     }
 
     public static function loadproject($id = 0) {
-        if($id == 0) {
+        if ($id == 0) {
             return false;
         }
         $results = database::getRow('projecten', ['id'], 's', [$id]);
@@ -25,17 +25,16 @@ class Projects{
 
     public static function addproject($target_dir, $target_file, $imageFileType, $filename, $name, $continue, $files, $title, $pgit, $plink) {
         $img = Projects::uploadimg($target_dir, $target_file, $imageFileType, $files);
-        if($filename and $name) {
+        if ($filename and $name) {
             $zip = Projects::uploadzip($filename, $name, $continue, $files);
 
-        }else{
+        } else {
             $zip = true;
         }
 
-
-        if($img == false) {
+        if (!$img) {
             echo "<script>alert('Sorry, there was an error uploading your img');</script>";
-        } else if($zip == false) {
+        } elseif (!$zip) {
             echo "<script>alert('Your .zip file was not uploaded and unpacked');</script>";
         } else {
             $git = "";
@@ -47,19 +46,16 @@ class Projects{
 
             $link = "";
             if ($plink == "") {
-                if (isset($zip)) {
-                    $link = substr($zip, 0, -4) . "/index";
-                }
+                $link = substr($zip, 0, -4) . "/index";
             } else {
                 $link = $_POST["link"];
             }
-
             projects::dbaddproject($title, $git, $link, $img);
         }
     }
 
     public static function dbaddproject($name, $git, $link, $file) {
-    Database::add('projecten', ['name','github','path','img'], 'ssss', [$name, $git, $link, $file]);
+        Database::add('projecten', ['name', 'github', 'path', 'img'], 'ssss', [$name, $git, $link, $file]);
     }
 
     public static function uploadimg($target_dir, $target_file, $imageFileType, $files) {
@@ -81,36 +77,20 @@ class Projects{
             $uploadOk = 0;
         }
 
-        // Allow certain file formats
-        if (
-            $imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-            && $imageFileType != "gif"
-        ) {
-            echo "<script>alert('Sorry, only JPG, JPEG, PNG & GIF files are allowed');</script>";
-            $uploadOk = 0;
-            $target_file = "";
-        } else{
-
-            // Check if $uploadOk is set to 0 by an error
-            if ($uploadOk == 0) {
-                // if everything is ok, try to upload file
+        // Check if $uploadOk is not set to 0 by an error
+        if ($uploadOk != 0) {
+            if (move_uploaded_file($files["img"]["tmp_name"], $target_file)) {
+                return $target_file;
             } else {
-                if (move_uploaded_file($files["img"]["tmp_name"], $target_file)) {
-                    return $target_file;
-//                    echo "<script>alert('The img has been uploaded.');</script>";
-                } else {
-                    return false;
-//                    echo "<script>alert('Sorry, there was an error uploading your img');</script>";
-                }
+                return false;
             }
         }
-
     }
 
     public static function uploadzip($filename, $name, $continue, $files) {
         if ($files["zip_file"]["name"]) {
             $source = $files["zip_file"]["tmp_name"];
-            $type = $files["zip_file"]["type"];
+            $type   = $files["zip_file"]["type"];
 
             $accepted_types = array('application/zip', 'application/x-zip-compressed', 'multipart/x-zip', 'application/x-compressed');
             foreach ($accepted_types as $mime_type) {
@@ -123,21 +103,18 @@ class Projects{
             if (!$continue) {
                 echo "<script>alert('The file you are trying to upload is not a .zip file. Please try again');</script>";
             } else {
-
-                $target_path = 'project/' . $filename;  // change this to the correct site path
+                $target_path = 'project/' . $filename;
                 if (move_uploaded_file($source, $target_path)) {
                     $zip = new ZipArchive();
-                    $x = $zip->open($target_path);
+                    $x   = $zip->open($target_path);
                     if ($x === true) {
-                        $zip->extractTo('project/'); // change this to the correct site path
+                        $zip->extractTo('project/');
                         $zip->close();
-
                         unlink($target_path);
                     }
                     return $target_path;
                 } else {
                     return false;
-//                    echo "<script>alert('There was a problem with the upload. Please try again');</script>";
                 }
             }
         }
