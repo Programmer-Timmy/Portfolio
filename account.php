46fb9ee77ce130c81355cfc067df0cdc090ecab6
Tim-VD-K <tim.vanderkloet@gmail.com>
2023-01-26 12:10:57 +0100

    Update account.php

--- account.php
+++ account.php
@@ -1,14 +1,8 @@
 <?php
 include('requierd.php');
     if ($_POST) {
-        $account = $database::getRow('account', ['username'], 's', [$_POST['username']]);        
-        if (!isset($account['password'])) {
-            echo '<script>alert("Wrong username or password")</script>';
-        } elseif (password_verify($_POST['password'], $account['password'])) {
-            $_SESSION['access'] = "logged";
-        } else {
-            echo '<script>alert("Wrong username or password")</script>';
-        }
+        $return = accounts::Login($_POST['password'], $_POST['username']);
+        echo $return;
     }
 
 ?>
