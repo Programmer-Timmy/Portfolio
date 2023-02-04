4b1a0c6933cfa933bd418ff75fd135b0955a4abb
Tim-VD-K <tim.vanderkloet@gmail.com>
2023-02-05 00:35:25 +0100

    Update contact.php

--- contact.php
+++ contact.php
@@ -20,10 +20,10 @@
             </a>
             <nav id="nav">
                 <ul>
-                    <li><a href="index.php">Home</a></li>
-                    <li><a href="about.php">About</a></li>
+                    <li><a href="index">Home</a></li>
+                    <li><a href="about">About</a></li>
                     <li><a href="">Contact</a></li>
-                    <li><a href="projects.php">Projects</a></li>
+                    <li><a href="projects">Projects</a></li>
                 </ul>
             </nav>
         </div>
