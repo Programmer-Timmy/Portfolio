961c5f56bdc84e13d593e48a54d7a7c97cab7fa4
Tim-VD-K <tim.vanderkloet@gmail.com>
2023-02-05 00:27:11 +0100

    Update add-project.php

--- add-project.php
+++ add-project.php
@@ -41,6 +41,10 @@ if ($_POST) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/styles.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <?php if (!isset($_SESSION["admin"])) {
+        echo '<li><a href="users">Users</a></li>';
+    } ?>
+
     <title>test</title>
 </head>
 
