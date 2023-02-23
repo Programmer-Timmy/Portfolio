b15addf1a0541d7f76b792237a058a4edcf8691d
Tim-VD-K <tim.vanderkloet@gmail.com>
2023-02-23 16:51:23 +0100

    Update add-project.php

--- admin/add-project.php
+++ admin/add-project.php
@@ -46,7 +46,7 @@ if ($_POST) {
 </head>
 
 <body>
-    <?php require_once 'header.php';?>
+    <?php require_once 'header.php'; ?>
     <div class="admin">
         <form method="post" enctype="multipart/form-data">
             Naam van het project:
@@ -62,6 +62,6 @@ if ($_POST) {
         </form>
     </div>
 </body>
-<script src="js/nav.js"></script>
+<script src="../js/nav.js"></script>
 
 </html>
