82a4a1c81dfa1d78123621fdaceec0101ccb301e
Programmer-Timmy <122127801+Programmer-Timmy@users.noreply.github.com>
2023-03-16 11:01:06 +0100

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -9,7 +9,6 @@ if ($_POST) {
 
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -41,5 +40,4 @@ if ($_POST) {
 
 </body>
 <script src="../js/nav.js"></script>
-
 </html>
