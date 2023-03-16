1487bf91883618ff9cfa9f1cd5845c3efe85ce28
Programmer-Timmy <122127801+Programmer-Timmy@users.noreply.github.com>
2023-03-16 11:01:18 +0100

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,6 @@ $projects = Projects::loadprojects("3");
 
 <!DOCTYPE html>
 <html lang="nl">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -14,8 +13,6 @@ $projects = Projects::loadprojects("3");
     <link rel="stylesheet" href="css/styles.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <title>Home</title>
-
-
 </head>
 
 <body>
@@ -28,18 +25,16 @@ $projects = Projects::loadprojects("3");
             <nav id="nav">
                 <ul>
                     <li><a href="">Home</a></li>
-                    <li><a href="about.php">About</a></li>
-                    <li><a href="contact.php">Contact</a></li>
-                    <li><a href="projects.php">Projects</a></li>
+                    <li><a href="about">About</a></li>
+                    <li><a href="contact">Contact</a></li>
+                    <li><a href="projects">Projects</a></li>
                 </ul>
             </nav>
         </div>
     </header>
-    <!-- welcome txt -->
     <div class="welcome">
         <h1>Welcome!</h1>
     </div>
-    <!-- 3 projecten -->
     <div class="borderp">
         <?php
         if ($projects) {
@@ -60,7 +55,6 @@ $projects = Projects::loadprojects("3");
         }
         ?>
     </div>
-
 </body>
 <script src="js/nav.js"></script>
 
