a3cd68094f13263bdd387bda1956eaa899180998
Programmer-Timmy <122127801+Programmer-Timmy@users.noreply.github.com>
2023-03-16 11:01:26 +0100

    Update requierd.php

--- requierd.php
+++ requierd.php
@@ -1,14 +1,16 @@
 <?php
 error_reporting(E_ALL ^ E_DEPRECATED);
-
+set_exception_handler(function($exception){
+    echo "<h1> oops er is iets fout gegaan</h1>";
+});
 session_start();
+
+
 
 include 'classes/projects.php';
 include 'classes/database.php';
 include 'classes/databaseUtilities.php';
 include 'classes/accounts.php';
-
-
 
 $dbhost = 'localhost';
 $dbuser = 'root';
