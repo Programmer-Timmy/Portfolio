696709c9388af0acbdb8a7f771ef5c9154410878
Programmer-Timmy <122127801+Programmer-Timmy@users.noreply.github.com>
2023-03-24 11:28:58 +0100

    Update requierd.php

--- requierd.php
+++ requierd.php
@@ -1,11 +1,16 @@
 <?php
 error_reporting(E_ALL ^ E_DEPRECATED);
+error_reporting(E_ERROR | E_PARSE);
 set_exception_handler(function($exception){
     echo "<h1> oops er is iets fout gegaan</h1>";
 });
 session_start();
 
-
+if (isset($_SESSION['discard_after']) && time() > $_SESSION['discard_after']) {
+    session_unset();
+    session_destroy();
+    session_start();
+}
 
 include 'classes/projects.php';
 include 'classes/database.php';
