3189a6dcccf64bea38a5697d4f4acf9f513edf4d
Programmer-Timmy <122127801+Programmer-Timmy@users.noreply.github.com>
2023-03-27 09:52:40 +0200

    Update logout.php

--- admin/logout.php
+++ admin/logout.php
@@ -1,6 +1,11 @@
 <?php 
 session_start();
 if (isset($_SESSION)); {
+    session_unset();
     session_destroy();
-    header('location: ../admin');
+    echo('<script>if (confirm("Wil je opnieuw inloggen?") == true) {
+        window.location.replace("../admin");
+        } else {
+        window.location.replace("/");
+        }</script>');
 }
