6b2134f0f1d1581b958d745694be5f96df3ca3db
Programmer-Timmy <122127801+Programmer-Timmy@users.noreply.github.com>
2023-05-15 12:15:17 +0200

    adding 404 page

---
+++ 404.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>Home</title>
+</head>
+
+<body>
+<?php require_once 'header.html' ?>
+<div class="welcome">
+    <h1>Oops! Page Not Found</h1>
+    <h2 style="font-size: 20px; color: #777777">We're sorry, but the page you are looking for could not be found.</h2>
+    <h2 style="font-size: 20px; color: #777777">You may have typed the URL incorrectly or the page has been moved or deleted.</h2>
+    <h2 style="font-size: 20px; color: #777777">Please check the URL or navigate back to the <a style="color: #55d6aa; text-decoration: none;" href="/home">home page</a>.</h2>
+</div>
+</body>
+<script src="js/nav.js"></script>
+
+</html>
+
+
