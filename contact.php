6222a0da4db4ce70abe50d1ab4e19260b19ef3dc
Programmer-Timmy <122127801+Programmer-Timmy@users.noreply.github.com>
2023-05-22 13:00:13 +0200

    added more map structure

--- contact.php
+++ contact.php
@@ -21,19 +21,17 @@
             <h2>Thank you for visiting my portfolio page! I'd be happy to connect with you using any of the following methods:</h2><br>
             <h3>
                 <ul>
-                    <li>Email: <a href="mailto:tim.vanderkloet@gmail.com">Tim.vanderkloet@gmail.com</a></li>
-                    <li>Phone number: <a href="tel:0643612148">+31 643612148</a></li>
-
+                    <li>Email: <a href="mailto:Tim.vanderkloet@gmail.com">Tim.vanderkloet@gmail.com</a></li>
+                    <li>Phone number: <a href="tel:+31643612148">+31 643612148</a></li>
                     <li>Social media:
-                        <ul style="padding-left: 20px;margin-top: 5px;">
-                            <li>LinkedIn: <a href="https://nl.linkedin.com/in/tim-van-der-kloet-110395235">Tim van der Kloet</a></li>
-                            <li>GitHub: <a href="https://github.com/Programmer-Timmy">Programmer Timmy</a></li>
+                        <ul style="padding-left: 20px; margin-top: 5px;">
+                            <li>LinkedIn: <a href="https://www.linkedin.com/in/tim-van-der-kloet">Tim van der Kloet</a></li>
+                            <li>GitHub: <a href="https://github.com/Programmer-Timmy">Programmer-Timmy</a></li>
                         </ul>
-                    </li><br>
-
-                    Feel free to reach out to me for any questions, feedback or collaboration opportunities. I look forward to hearing from you!
+                    </li>
                 </ul>
             </h3><br>
+            <h2>I'd love to hear from you, whether it's for questions, feedback, or collaboration opportunities. Don't hesitate to reach out—I'm here to help!</h2>
         </div>
         <div>
             <img src="img/profielfoto.JPG" id="foto" alt="">
