b04f6602572e89c6528e30f1a97445c5477fe458
Programmer-Timmy <122127801+Programmer-Timmy@users.noreply.github.com>
2024-05-31 22:35:30 +0200

    added fav icons

--- private/views/templates/header.php
+++ private/views/templates/header.php
@@ -25,6 +25,19 @@ function getPageTitle()
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo getPageTitle(); ?></title>
+    <!-- fav icons -->
+    <link rel="apple-touch-icon" sizes="180x180" href="/img/favicoins/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/img/favicoins/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/img/favicoins/favicon-16x16.png">
+    <link rel="manifest" href="/img/favicoins/site.webmanifest">
+    <link rel="mask-icon" href="/img/favicoins/safari-pinned-tab.svg" color="#5bbad5">
+    <link rel="shortcut icon" href="/img/favicoins/favicon.ico">
+    <meta name="apple-mobile-web-app-title" content="Tim van der Kloet">
+    <meta name="application-name" content="Tim van der Kloet">
+    <meta name="msapplication-TileColor" content="#00aba9">
+    <meta name="msapplication-config" content="/img/favicoins/browserconfig.xml">
+    <meta name="theme-color" content="#ffffff">
+
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
