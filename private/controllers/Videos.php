339068566117b3318ed1d5ed5972edc5483c2e12
Programmer-Timmy <122127801+Programmer-Timmy@users.noreply.github.com>
2024-05-31 21:56:16 +0200

    format fixes

--- private/controllers/Videos.php
+++ private/controllers/Videos.php
@@ -33,7 +33,7 @@ class Videos
     public static function add(): void
     {
         try {
-            $env = parse_ini_file('../portfolio.env');
+            $env = parse_ini_file(__DIR__ . '/../portfolio.env');
             if (!$env) {
                 throw new Exception('Failed to parse environment file.');
             }
