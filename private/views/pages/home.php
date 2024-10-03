77906eed19a3529b473515c78e680f53754dd3e8
Programmer-Timmy <122127801+Programmer-Timmy@users.noreply.github.com>
2024-10-03 23:53:33 +0200

    text added to my home page

--- private/views/pages/home.php
+++ private/views/pages/home.php
@@ -1,38 +1,45 @@
 <?php
 $projects = Projects::loadProjects("3");
 ?>
-
-<div class="welcome">
-    <h1>Welcome!</h1>
-</div>
 <div class="container" style="max-width: 90%">
-    <div class="borderp row" style="color: red">
+    <div class="welcome">
+        <h1>Welcome!</h1>
+
+        <div class="text px-lg-5">
+            <p>My name is Tim and I am a developer. I have experience with PHP, C#, Python, TypeScript, SCSS, and more.
+                I have worked on multiple projects, some of which you can see below. If you want to see more projects, click the button below.
+            </p>
+        </div>
+        <a href="projects" class="btn btn-primary"> View More Projects</a>
+    </div>
+    <div class="borderp" style="color: red">
         <?php if ($projects): ?>
-            <?php foreach ($projects as $project): ?>
-                <div class="col-md-6 col-lg-4">
-                    <div class="project-home">
-                        <div class="position-relative">
-                            <?php if ($project->pinned): ?>
-                                <i class="pinned position-absolute translate-middle p-2 bg-success border border-light rounded-circle fa-solid fa-thumbtack"></i>
-                            <?php endif; ?>
-                            <a href="project?id=<?= $project->id ?>">
-                                <img src="<?php echo $project->img; ?>" class="img-size" alt="">
-                            </a>
-                        </div>
-                        <h1>
-                            <?php if ($project->github): ?>
-                                <a class="github text-decoration-none" href="<?php echo $project->github; ?>">
-                                    <i class="fa fa-github" aria-hidden="true"></i>
+            <div class="row">
+                <?php foreach ($projects as $project): ?>
+                    <div class="col-md-6 col-lg-4">
+                        <div class="project-home">
+                            <div class="position-relative">
+                                <?php if ($project->pinned): ?>
+                                    <i class="pinned position-absolute translate-middle p-2 bg-success border border-light rounded-circle fa-solid fa-thumbtack"></i>
+                                <?php endif; ?>
+                                <a href="project?id=<?= $project->id ?>">
+                                    <img src="<?php echo $project->img; ?>" class="img-size" alt="">
                                 </a>
-                            <?php endif; ?>
-                            <?php echo $project->name; ?>
-                        </h1>
+                            </div>
+                            <h1>
+                                <?php if ($project->github): ?>
+                                    <a class="github text-decoration-none" href="<?php echo $project->github; ?>">
+                                        <i class="fa fa-github" aria-hidden="true"></i>
+                                    </a>
+                                <?php endif; ?>
+                                <?php echo $project->name; ?>
+                            </h1>
+                        </div>
                     </div>
-                </div>
-            <?php endforeach; ?>
+                <?php endforeach; ?>
+            </div>
         <?php else: ?>
             <h1>Helaas geen projecten</h1>
         <?php endif; ?>
     </div>
 </div>
-<?php require_once '../private/views/Popups/ProjectPopup.php'; ?>
