d073a4eea5460503cf1b2d49aac71ed5bc095b9b
Programmer-Timmy <122127801+Programmer-Timmy@users.noreply.github.com>
2024-10-04 16:22:31 +0200

    biggerrr

--- private/views/templates/header.php
+++ private/views/templates/header.php
@@ -42,7 +42,7 @@ function getPageTitle()
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <!-- Custom CSS -->
-    <link rel="stylesheet" href="/css/styles.css">
+    <link rel="stylesheet" href="/css/styles.css?v=1.0">
     <!-- ajax -->
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"
             integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
